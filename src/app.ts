a329cfe3e0a5ffefd7166ba9a2a8555ef05fbc90
Saif Maamun <sadifmamun@gmail.com>
2023-05-31 19:38:34 +0600

    eslint, prettier, husky, lint-staaged

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,15 @@
-import express, { Application, Request, Response } from 'express';
-import cors from "cors"
-const app:Application = express()
-const port = 3000
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+const app: Application = express()
 
-
-
-app.use(cors());
+app.use(cors())
 
 // parser
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-
-
-
-app.get('/', (req:Request, res:Response) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('working successfully')
 })
 
-
-
-export default app;
+export default app
--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,19 @@
-import mongoose from 'mongoose';
-import config from './config';
-import app from './app';
+import mongoose from 'mongoose'
+import config from './config'
+import app from './app'
 
 async function server() {
-    try{
-        await mongoose.connect(config.database_url as string);
-        console.log('connected to MongoDB')
-        app.listen(config.port, () => {
-            console.log(`Application listening on port ${config.port}`)
-          })
-    }catch(e){
-        console.log('error connecting', e)
-    }
-  
-    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log('connected to MongoDB')
+    app.listen(config.port, () => {
+      console.log(`Application listening on port ${config.port}`)
+    })
+  } catch (e) {
+    console.log('error connecting', e)
   }
 
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
 
-  server();
+server()
