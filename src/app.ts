68e46fd02c3d371ead0d5eaa8c49fcd837becb52
Saif Maamun <sadifmamun@gmail.com>
2023-06-01 20:34:32 +0600

    schema model service

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import usersRouter from './app/modules/users/users.route'
 const app: Application = express()
 
 app.use(cors())
@@ -7,6 +8,9 @@ app.use(cors())
 // parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+// application routes
+app.use('/api/v1/users', usersRouter)
 
 app.get('/', (req: Request, res: Response) => {
   res.send('working successfully')
