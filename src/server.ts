dd5415e367484ecb10e0a44dca00179d4ee5a8a7
Saif Maamun <sadifmamun@gmail.com>
2023-06-08 00:28:58 +0600

    info and error logger

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
 import mongoose from 'mongoose'
 import config from './config'
 import app from './app'
+import logger from './shared/logger'
 
 async function server() {
   try {
     await mongoose.connect(config.database_url as string)
-    console.log('connected to MongoDB')
+    logger.info('connected to MongoDB')
     app.listen(config.port, () => {
-      console.log(`Application listening on port ${config.port}`)
+      logger.info(`Application listening on port ${config.port}`)
     })
   } catch (e) {
-    console.log('error connecting', e)
+    logger.error('error connecting', e)
   }
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
