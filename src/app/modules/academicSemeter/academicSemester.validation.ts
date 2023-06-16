3b608fe05545f67d6fd2484eecb457dfa111d172
Saif Maamun <sadifmamun@gmail.com>
2023-06-16 18:36:47 +0600

    academic semester building

---
+++ src/app/modules/academicSemeter/academicSemester.validation.ts
@@ -0,0 +1,56 @@
+import { z } from 'zod';
+
+const createAcademicSemesterZodSchema = z.object({
+  body: z.object({
+    title: z.enum(['Autumn', 'Summer', 'Fall'], {
+      required_error: 'Title is required',
+    }),
+    year: z.number({
+      required_error: 'Year is required',
+    }),
+    code: z.enum(['01', '02', '03']),
+    startMonth: z.enum(
+      [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December',
+      ],
+      {
+        required_error: 'StartMonth is required',
+      }
+    ),
+    endMonth: z.enum(
+      [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December',
+      ],
+      {
+        required_error: 'End Month is required',
+      }
+    ),
+  }),
+});
+// await createZodUserSchema.parseAsync(req);
+
+export const AcademicSemesterValidation = {
+  createAcademicSemesterZodSchema,
+};
--- src/app/modules/academicSemeter/academicSemesterModel.ts
+++ src/app/modules/academicSemeter/academicSemesterModel.ts
@@ -4,12 +4,27 @@ import {
   AcademicSemesterModel,
 } from './academicSemester.interface';
 
+const Month = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
 // creating schema
 const academicSemesterSchema = new Schema<IAcademicSemester>(
   {
     title: {
       type: String,
       required: true,
+      enum: ['Autumn', 'Summer', 'Fall'],
     },
     year: {
       type: Number,
@@ -18,14 +33,17 @@ const academicSemesterSchema = new Schema<IAcademicSemester>(
     code: {
       type: String,
       required: true,
+      enum: ['01', '02', '03'],
     },
     startMonth: {
       type: String,
       required: true,
+      enum: Month,
     },
     endMonth: {
       type: String,
       required: true,
+      enum: Month,
     },
   },
   {
