a27bb13daa0cf615627529acd62c0d81b8532176
Saif Maamun <sadifmamun@gmail.com>
2023-06-17 02:25:05 +0600

    academic semester code checker

--- src/app/modules/academicSemeter/academicSemesterModel.ts
+++ src/app/modules/academicSemeter/academicSemesterModel.ts
@@ -9,7 +9,7 @@ import {
   academicSemesterTitles,
 } from './academicSemester.constant';
 import ApiError from '../../../errors/ApiErrors';
-import status from 'http-status';
+import httpStatus from 'http-status';
 
 // creating schema
 const academicSemesterSchema = new Schema<IAcademicSemester>(
@@ -50,7 +50,7 @@ academicSemesterSchema.pre('save', async function (next) {
     year: this.year,
   });
   if (isExist) {
-    throw new ApiError(status.CONFLICT, 'Academic Semester already exists');
+    throw new ApiError(httpStatus.CONFLICT, 'Academic Semester already exists');
   }
   next();
 });
