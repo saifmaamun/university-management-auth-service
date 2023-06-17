85b949c603e41d0f88b3618de2f729f82c7f72f8
Saif Maamun <sadifmamun@gmail.com>
2023-06-17 09:57:04 +0600

    Not Found API handled

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
-import express, { Application } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import routes from './app/routes';
+import httpStatus from 'http-status';
 // import ApiError from './errors/ApiErrors';
 const app: Application = express();
 
@@ -16,4 +17,18 @@ app.use('/api/v1/', routes);
 // global error handler
 app.use(globalErrorHandler);
 
+// handle not found route
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: 'Not found',
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: 'API not Found',
+      },
+    ],
+  });
+  next();
+});
 export default app;
