bd1b839ab1df81cc9b15c0d9d8c077d2a4ff2263
Saif Maamun <sadifmamun@gmail.com>
2023-06-17 17:49:42 +0600

    find singleone by id

--- src/app/modules/academicSemeter/academicSemester.controller.ts
+++ src/app/modules/academicSemeter/academicSemester.controller.ts
@@ -44,7 +44,23 @@ const getAllSemesters = catchAsync(
   }
 );
 
+const getSingleSemester = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const id = req.params.id;
+
+    const result = await AcademicSemesterService.getSingleSemester(id);
+    sendResponse<IAcademicSemester>(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'Semester retreated successfully',
+      data: result,
+    });
+    next();
+  }
+);
+
 export const AcademicSemesterController = {
   createSemester,
   getAllSemesters,
+  getSingleSemester,
 };
